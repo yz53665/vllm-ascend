d287aded2e7dac9e2a1df39efab92696a38da27a
QiuRuiqi <875423353@qq.com>
2026-02-06 16:50:08 +0800

    fix bug of backend __init__.py file.

--- vllm_ascend/distributed/mooncake_user/backend/__init__.py
+++ vllm_ascend/distributed/mooncake_user/backend/__init__.py
@@ -1,7 +1,8 @@
 from typing import Dict, Type
-from backend import Backend
-from memcache_backend import MemcacheBackend
-from mooncake_backend import MooncakeBackend
+from .backend import Backend
+from .memcache_backend import MemcacheBackend
+from .mooncake_backend import MooncakeBackend
+
 
 backend_map: Dict[str, Type[Backend]] = {
     "mooncake": MooncakeBackend,
